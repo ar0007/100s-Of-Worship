3403a549f2c29179131464363d89642c3f10447d
ar0007 <ariysn.shihab3345@gmail.com>
2022-01-22 23:05:41 +0600

    initialcommit

--- script.js
+++ script.js
@@ -54,28 +54,71 @@ function closePopup() {
 //     myFunction3();
 // }, 66000);
 
-const firstPromise = new Promise((resolve, reject) => {
-    for (let i = 0; i <= 33; i++) {
-        setTimeout(() => {
-            count.innerHTML = i;
-        }, i * 1000);
-    }
-    resolve();
-});
+// const firstPromise = new Promise((resolve, reject) => {
+//     for (let i = 0; i <= 33; i++) {
+//         setTimeout(() => {
+//             count.innerHTML = i;
+//         }, i * 1000);
+//     }
+//     resolve();
+// });
 
-const secondPromise = new Promise((resolve, reject) => {
-    for (let i = 0; i < 33; i++) {
-        setTimeout(() => {
-            count2.innerHTML = i;
-        }, i * 1000);
-    }
-    resolve();
-});
+// const secondPromise = new Promise((resolve, reject) => {
+//     for (let i = 0; i < 33; i++) {
+//         setTimeout(() => {
+//             count2.innerHTML = i;
+//         }, i * 1000);
+//     }
+//     resolve();
+// });
 
-//run first promise
+// //run first promise
 
-firstPromise.then((response) => {
-    //run second promise after first promise succeed
+// firstPromise.then((response) => {
+//     //run second promise after first promise succeed
 
-    secondPromise.then((response) => {});
-});
+//     secondPromise.then((response) => {});
+// });
+
+// =======================================
+
+// `timer` accepts a count, and the element
+// to apply the count to
+function timer(count, el) {
+    // Return a promise that basically says:
+    // once I'm done doing this work, resolve,
+    // and then the event queue can
+    // get on with the next thing
+    return new Promise((resolve) => {
+        // So we create a loop that logs the numbers
+        // in our element up to the count we specified.
+        // and when that number is reached, resolve the promise
+        function loop(n = 0) {
+            // If our current `n` value is <= count
+            if (n <= count) {
+                // Set the content of the element
+                el.textContent = n;
+
+                // Call `loop` again after a second
+                // with an incremented `n` value
+                setTimeout(loop, 1000, ++n);
+
+                // Otherwise resolve the promise
+            } else {
+                resolve();
+            }
+        }
+
+        loop();
+    });
+}
+
+// And now we just await each resolved promise
+async function main() {
+    await timer(33, count);
+    await timer(33, count2);
+    await timer(34, count3);
+    showPopup();
+}
+
+main();
