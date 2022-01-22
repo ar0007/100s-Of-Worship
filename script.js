05ca13e3c15f2fc0de6a6c0b28545bd47b1ebb80
ar0007 <ariysn.shihab3345@gmail.com>
2022-01-22 22:29:49 +0600

    initialcommit

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const count = document.getElementById(`counter1`);
 const count2 = document.getElementById(`counter2`);
 const count3 = document.getElementById(`counter3`);
+const popupButton = document.getElementsByClassName(`popup-colse-button`);
+
+// pop up function
+function showPopup() {
+    popup.style.transform = "scale(1)";
+}
+// pop up close
+function closePopup() {
+    popup.style.transform = "scale(0)";
+}
 
 function myFunction1() {
     for (let i = 1; i <= 33; i++) {
         setTimeout(() => {
             count.innerHTML = i;
+            if (i === 34) {
+                myFunction2();
+            }
         }, i * 1000);
     }
 }
@@ -17,20 +30,26 @@ function myFunction2() {
     for (let i = 1; i <= 33; i++) {
         setTimeout(() => {
             count2.innerHTML = i;
+            if (i === 34) {
+                myFunction2();
+            }
         }, i * 1000);
     }
 }
-setTimeout(function() {
-    myFunction2();
-}, 33000);
+// setTimeout(function() {
+//     myFunction2();
+// }, 33000);
 
 function myFunction3() {
     for (let i = 1; i <= 34; i++) {
         setTimeout(() => {
             count3.innerHTML = i;
+            if (i === 35) {
+                showPopup();
+            }
         }, i * 1000);
     }
 }
-setTimeout(function() {
-    myFunction3();
-}, 66000);
+// setTimeout(function() {
+//     myFunction3();
+// }, 66000);
