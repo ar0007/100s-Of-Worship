98d345a2d762986ef6763e0e3d4f5e85d9268422
ar0007 <ariysn.shihab3345@gmail.com>
2022-01-22 22:49:57 +0600

    initialcommit

--- script.js
+++ script.js
@@ -12,44 +12,70 @@ function closePopup() {
     popup.style.transform = "scale(0)";
 }
 
-function myFunction1() {
-    for (let i = 1; i <= 33; i++) {
-        setTimeout(() => {
-            count.innerHTML = i;
-            if (i === 34) {
-                myFunction2();
-            }
-        }, i * 1000);
-    }
-}
-myFunction1();
+// function myFunction1() {
+//     for (let i = 1; i <= 33; i++) {
+//         setTimeout(() => {
+//             count.innerHTML = i;
+//             if (i === 34) {
+//                 myFunction2();
+//             }
+//         }, i * 1000);
+//     }
+// }
+// myFunction1();
 
-// second one
+// // second one
 
-function myFunction2() {
-    for (let i = 1; i <= 33; i++) {
-        setTimeout(() => {
-            count2.innerHTML = i;
-            if (i === 34) {
-                myFunction3();
-            }
-        }, i * 1000);
-    }
-}
-// setTimeout(function() {
-//     myFunction2();
-// }, 33000);
+// function myFunction2() {
+//     for (let i = 1; i <= 33; i++) {
+//         setTimeout(() => {
+//             count2.innerHTML = i;
+//             if (i === 34) {
+//                 myFunction3();
+//             }
+//         }, i * 1000);
+//     }
+// }
+// // setTimeout(function() {
+// //     myFunction2();
+// // }, 33000);
 
-function myFunction3() {
-    for (let i = 1; i <= 34; i++) {
-        setTimeout(() => {
-            count3.innerHTML = i;
-            if (i === 35) {
-                showPopup();
-            }
-        }, i * 1000);
-    }
-}
+// function myFunction3() {
+//     for (let i = 1; i <= 34; i++) {
+//         setTimeout(() => {
+//             count3.innerHTML = i;
+//             if (i === 35) {
+//                 showPopup();
+//             }
+//         }, i * 1000);
+//     }
+// }
 // setTimeout(function() {
 //     myFunction3();
 // }, 66000);
+
+const firstPromise = new Promise((resolve, reject) => {
+    for (let i = 0; i <= 33; i++) {
+        setTimeout(() => {
+            count.innerHTML = i;
+        }, i * 1000);
+    }
+    resolve();
+});
+
+const secondPromise = new Promise((resolve, reject) => {
+    for (let i = 0; i < 33; i++) {
+        setTimeout(() => {
+            count2.innerHTML = i;
+        }, i * 1000);
+    }
+    resolve();
+});
+
+//run first promise
+
+firstPromise.then((response) => {
+    //run second promise after first promise succeed
+
+    secondPromise.then((response) => {});
+});
